132b13c1e4fbc4e14cbd71aa84463d98ac35f532
Anthony Lee <anthonylee1994@users.noreply.github.com>
2024-12-18 10:16:15 +0800

    feat: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,21 +7,33 @@ import {AuthModule} from "./auth/auth.module";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {TagsModule} from "./tags/tags.module";
 import {MetaOptionsModule} from "./meta-options/meta-options.module";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import appConfig from "./config/app.config";
+import databaseConfig from "./config/database.config";
+import environmentValidation from "./config/environment.validation";
+
+const ENV = process.env.NODE_ENV || "development";
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: !ENV ? ".env" : `.env.${ENV}`,
+            load: [appConfig, databaseConfig],
+            validationSchema: environmentValidation,
+        }),
         TypeOrmModule.forRootAsync({
-            imports: [],
-            inject: [],
-            useFactory: () => ({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
                 type: "postgres",
-                host: "localhost",
-                port: 5432,
-                username: "postgres",
-                password: "",
-                database: "nestjs_learning",
-                autoLoadEntities: true,
-                synchronize: true,
+                host: configService.get("database.host"),
+                port: configService.get("database.port"),
+                username: configService.get("database.user"),
+                password: configService.get("database.password"),
+                database: configService.get("database.name"),
+                autoLoadEntities: configService.get("database.autoLoadEntities"),
+                synchronize: configService.get("database.synchronize"),
             }),
         }),
         UsersModule,
